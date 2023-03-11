1a685253e5a8c2493323e6eff2f5d746140ddeb9
Melanie Cooper <melscoop@github.com>
2023-03-11 14:54:17 -0500

    adding stats commands

--- snorem_diags.sh
+++ snorem_diags.sh
@@ -2,11 +2,24 @@
 
 # Welcome message
 echo -e "Zzzz... Hello there. I'm a sleepy sloth, but I'll try to help you with your system diagnostics. Zzzz...\n"
+echo ""
 
 # Display system information
 echo "System Information:"
 echo "--------------------"
 hostnamectl
+echo ""
+
+# Display system throughput IOPs
+
+echo "System throughput IOPS:"
+iostat -d -x 1 3
+echo ""
+
+# Display CPU load
+
+echo "CPU Load:"
+htop -n 1 -p $(pgrep -d',' -x -n -v htop)
 echo ""
 
 # Display system load
@@ -15,16 +28,22 @@ echo "-------------"
 uptime
 echo ""
 
-# Display disk usage
-echo "Disk Usage:"
-echo "-----------"
-df -h
-echo ""
-
 # Display memory usage
 echo "Memory Usage:"
 echo "-------------"
 free -h
+echo ""
+
+# Display and rogue processes
+
+echo "Rogue Processes:"
+htop
+echo ""
+
+# Display disk usage
+echo "Disk Usage:"
+echo "-----------"
+df -h
 echo ""
 
 # Display network information
